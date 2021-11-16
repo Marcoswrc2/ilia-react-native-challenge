98a2942ab1fcd606ab6cb07f7df15c071f980529
Marcoswrc2 <marcoswrc2@gmail.com>
2021-11-16 10:24:40 -0300

    Fix: remove unused code

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import {BoxCard, Title, ContentImage, BoxTitle} from './styles';
 import {IMAGE_BASE_URL, IMAGE_SIZE} from '../../services/api';
-import {useNavigation} from '@react-navigation/native';
-import {RouteNames} from '../../routes/types';
 
 const Card = ({title, posterPath, onPress}) => {
   return (
